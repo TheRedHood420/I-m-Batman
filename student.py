b5423fb39823c06dfb6a480de29d8e4c264de096
TheRedHood420 <112005163+TheRedHood420@users.noreply.github.com>
2023-05-08 15:42:23 +0000

    batman

--- student.py
+++ student.py
@@ -1,1 +1,155 @@
+Def main():
+    print(f"The BATMAN")
+    print(f"COMMISSIONER GORDON: Batman the riddle plans on detonating a atomic bomb, it's up to you stop him.")
+    atts = reset()
+    MENU = '''
+\t|Menu|
+S - Start
+X - Pause 
+Q - Quit
+I - Investigate
+P - Punch
+B - Batarang
+E - Explovises
+R - Reset'''
+    choice = 'a'
+    print("Batman Controls")
+    while choice != 'Q'
 
+    print(f"you've entered the warehouse, and are surrouded by an abyss darkness the only thing you can see is a small tape recorder.\n\nYou walk over and grab the tape recorder.")
+
+def Level_One(Question)
+
+    print(f"RIDDLER: Hello there dark knight, it's been a long while has it not. I presume You're here for my atomic bomb, I'm sorry but unfortunatley I cannot allow you to take it without a fight. So if you answer the riddle correctly you may enter. I'm guessing your silence means yes so let us begin")
+    print(f"For six months the second boy wonder was dead until he came back.\n\nThis hero is known for his phrase. You must put your trust in me, Sirius star is the brighest star, Sally had a bad day, Dylan said he would come in 10 minutes, space is the darkest area, you should end your day with glory.\n\nWho am I?\n\n")
+    Jason_Todd = input(Question)
+    While Jason_Todd =! "Green Lantern" and Jason_Todd =! "()"
+    Jason_Todd = Jason_Todd.title()
+    if Jason_Todd == Green Lantern:
+        print(f"well done cape crusader.")
+        return True
+    else: 
+        return False
+
+def Level_two(Option,One,Two,Three):
+    print("You enter through the door, The room looks almost reminiscent of a study. On the right side you see a large desk with some papers and pens on the desk. On the left side You see a large painting of the Joker riding a horse almost reminiscent of The Napoleon Crossing The Alps. on the right side of the room we see a large bookshelf. And directly in the middle of the room we see a table with three separate files and a vase of flowers.")
+    print("welcome to your next trap... I'm mean room. mwhahahahahahahahahahahahahahahahahahahahahahahahahahahahahahahaha\n now if you look at the desk you'll find 3 envelope pick one of the three and well good luck")
+    Nightwing = 0
+    Nightwing =  input(Option)
+   
+    while 1 > Nightwing or Nightwing > 3
+    Nightwing_is_int = False
+        try: 
+            Nightwing = int(Nightwing)
+            Nightwing_is_int = True
+        except: 
+            print("Uh Bats thats not a number.")
+            Nightwing = 0
+        if(Nightwing_is_int):
+            if 1 > Nightwing or Nightwing > 25:
+                print("its just one of the three, batman.")
+                one = "1"
+            elif 1 = Nightwing:
+                print("One may call it an insult, some may call it flattery, all the same it's a fun movie genre.")
+                Two_Face = ""
+                Two_Face = input("What am I?\n")
+                Two_Face = Two_Face.lower()
+                if Two_Face == parody:
+                    Two_Face = True
+                    return One
+                else:
+                    return False
+            elif 2 = Nightwing:
+                print("Some WHAT has its thorns, Some WHAT are blue.")
+                Harvey_Dent = ""
+                Harvey_Dent = input("What am I?\n")
+                Harvey_Dent = Harvey_Dent.lower()
+                if Harvey_Dent == flower or flowers:
+                    Harvey_Dent = True
+                    return Two
+                else: 
+                    return False
+            elif 3 = Nightwing:
+                print("He had one of those rare smiles with quality of eternal reassurance in it, that you come across four or five times in life.")
+                Peiguin = ""
+                Peiguin = input("Who am I?\n")
+                Peiguin = Peiguin.lower()
+                if Peiguin == gatsby 
+                    Peiguin = true 
+                    return Three
+                else:
+                    return False
+            Else
+                return False
+def Level_two_Painting(One):
+    print(f"RIDDLER: Well done cape crusader you figured out just 1 of many riddles, but now what must you do next, hurry the clock is ticking")
+    while ClayFace != "the painting" and ClayFace != "painting" and ClayFace != "paint" and ClayFace != "":
+        ClayFace = input("Enter where you'd like to investigate:\n")
+        clayface = clayface.lower()
+        if ClayFace == "the painting" or "painting" or "paint":
+           ClayFace = True
+           return One 
+        else:
+            print("Not quite")
+            return False
+
+def Level_two_Painting_exit(One):
+    print("Riddler: wow I'm impressed but how will you get through.")
+
+
+def Level_two_Flower(Two)
+    print(f"RIDDLER: Well done cape crusader you figured out just 1 of many riddles, but now what must you do next, hurry the clock is ticking")
+    while Mr.Freeze != "flower vase" and Mr.Freeze != "flower" and Mr.Freeze != "vase of flowers" and Mr.Freeze != "the vase of flower" and Mr.Freeze != "the flower vase" and Mr.Freeze != "":
+        Mr.Freeze = input("Enter where you'd like to investigate:\n")
+        Mr.Freeze = Mr.Freeze.lower()
+        if Mr.Freeze == "flower vase" or "flower" or "vase of flower" or "the vase of flower" or "the flower vase":
+            Mr.Freeze = True
+            return Two
+
+def Level_two_Flower_exit(Two)
+
+
+def Level_two_BookShelf(Three):
+    print(f"RIDDLER: Well done cape crusader you figured out just 1 of many riddles, but now what must you do next, hurry the clock is ticking")
+    while Hugo_Strange != "bookshelf" and Hugo_Strange != "the bookshelf" and Hugo_Strange != "":
+        Hugo_Strange = input("Enter where you'd like to investigate:\n")
+        Hugo_Strange = Hugo_Strange.lower()
+        if Hugo_Strange == "bookshelf" or "the bookshelf":
+            Hugo_Strange = True
+            return Three
+
+def Level_two_BookShelf_exit(Three):
+
+
+def Level_Three(One):
+
+
+def Level_Three(Three):
+
+def Level_Boss_Fight(One):
+#BOSS FIGHT WITH MR.FREEZE
+#USE POSSIBLE RANDOM SEQUENCE CONSICTING OF THROWING A BATARANG OR DODGING GO UNTIL THREE IN A ROW 
+
+def Level_Boss_Fight(Two):
+#BOSS FIGHT WITH KILLER CROC
+#USE RANDOM SEQUENCE OF JUST LEFT OR RIGHT GO FOR 2 OUT THREE TO PASS
+
+def Level_Boss_Fight(Three):
+#BOSS FIGHT WITH SCARWCROW
+
+def Level_Four(One):
+
+
+def Level_Four(Two):
+
+
+def Level_Four(Three):
+
+
+def Level_Five(One):
+
+
+def Level_Five(Three):
+
+
+def Level_Final_Boss(One,Two,Three):
